7ab529bc5ff2c53479aeaeeba2bf7e389e99b674
Victor Fuentes <vic.gfuentes@gmail.com>
2023-01-27 12:14:54 -0600

    initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express, { json } from "express";
+import items from "./items";
+
+const app = express();
+
+app.use(json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", async (_req, res) => {
+  res.json({ status: true, message: "express node.js app works" });
+});
+
+app.get("/items", (_req, res) => {
+  res.json({ status: true, message: "Fetched all items", data: items });
+});
+
+app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
